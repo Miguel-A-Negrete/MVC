7cb45f1863e9c3d7e23a7e7f1dac33761cae80cc
Jose Morales <149639682+Jose47Morales@users.noreply.github.com>
2024-04-28 14:19:06 -0500

    Add files via upload

---
+++ actasdigitales/controladores/RecordsController.php
@@ -0,0 +1,28 @@
+<?php
+class RecordController {
+    private $recordModel;
+
+    public function __construct($recordModel) {
+        $this->recordModel = $recordModel;
+    }
+
+    public function createRecord($id_record, $date_record, $start_time, $end_time, $affair, $responsible, $privacy, $relationship_record) {
+        return $this->recordModel->createRecord($id_record, $date_record, $start_time, $end_time, $affair, $responsible, $privacy, $relationship_record);
+    }
+
+    public function getAllRecords() {
+        return $this->recordModel->getAllRecords();
+    }
+    public function getRecordByID($id_record) {
+        return $this->recordModel->getRecordByID($id_record);
+    }
+
+    public function updateRecord($id_record, $date_record, $start_time, $end_time, $affair, $responsible, $privacy, $relationship_record) {
+        return $this->recordModel->updateRecord($id_record, $date_record, $start_time, $end_time, $affair, $responsible, $privacy, $relationship_record);
+    }
+
+    public function deleteRecord($id_record) {
+        return $this->recordModel->deleteRecord($id_record);
+    }
+}
+?>
