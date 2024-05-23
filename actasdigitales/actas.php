0ccb2206478c94962fe5ea293c3121ce0b9fbf37
Jose Morales <149639682+Jose47Morales@users.noreply.github.com>
2024-05-23 12:58:10 -0500

    Update actas.php

--- actasdigitales/actas.php
+++ actasdigitales/actas.php
@@ -2,21 +2,29 @@
 include_once './conexion/DB.php';
 include_once './controladores/RecordsController.php';
 include_once './modelos/RecordsModel.php';
+include_once './controladores/UsersController.php';
+include_once './modelos/UsersModel.php';
 session_start();
 if (isset($_SESSION['email'])) {
     $con = DB::getInstance();
     $recordController = new RecordController(new RecordModel($con));
+    $userController = new UserController(new UserModel($con));
+
+    $id = $userController->getUserID($_SESSION['email']);
     ?>
     <!DOCTYPE html>
     <html lang="es">
 
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
-        <link rel="stylesheet" href="./CSS/style.css">
         <link rel="stylesheet" href="./CSS/normalize.css">
+        <link rel="stylesheet" href="./CSS/style.css">
+        <title>Actas</title>
     </head>
+
     <body>
         <div class="grid-container">
 
@@ -29,7 +37,7 @@ if (isset($_SESSION['email'])) {
                     <span class="material-icons-outlined">notifications</span>
                     <span class="material-icons-outlined">email</span>
                     <span class="material-icons-outlined">account_circle</span>
-                    </div>
+                </div>
             </header>
             <aside id="sidebar">
                 <div class="sidebar-title">
@@ -41,7 +49,7 @@ if (isset($_SESSION['email'])) {
 
                 <ul class="sidebar-list">
                     <li class="sidebar-list-item">
-                        <a href="#" target="_blank">
+                        <a href="dashboard.php">
                             <span class="material-icons-outlined">dashboard</span> Panel
                         </a>
                     </li>
@@ -55,8 +63,8 @@ if (isset($_SESSION['email'])) {
                             <span class="material-icons-outlined">groups</span> Usuarios
                         </a>
                     </li>
-                    <li class="sidebar-list-item">
-                        <a href="#" target="_blank">
+                    <li class="sidebar-list-item active">
+                        <a href="actas.php">
                             <span class="material-icons-outlined">fact_check</span> Actas
                         </a>
                     </li>
@@ -74,194 +82,183 @@ if (isset($_SESSION['email'])) {
             </aside>
             <main class="main-container">
                 <div class="main-tittle">
-                    <h2>PANEL</h2>
+                    <h2>ACTAS</h2>
+                </div>
+
+                <div class="main-button">
+                    <button onclick="document.getElementById('modalForm').style.display='block'">Crear<svg
+                            class="material-icons-outlined" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                            <path
+                                d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                        </svg></button>
+                </div>
+
+                <div id="modalForm" class="modal">
+                    <div class="modal-content">
+                        <span class="close"
+                            onclick="document.getElementById('modalForm').style.display='none'">&times;</span>
+                        <form id="agregar-acta-form" action="index.php?route=records" method="post">
+                            <div class="form-group">
+                                <label for="asunto">Asunto:</label>
+                                <input type="text" id="asunto" name="affair" class="form-control" required>
+                            </div>
+                            <input type="hidden" id="responsable" name="responsible" class="form-control"
+                                value="<?php echo $id['id_user'] ?>">
+                            <div class="form-group">
+                                <label for="fecha">Fecha:</label>
+                                <input type="date" id="fecha" name="date_record" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="hora_inicio">Hora de Inicio:</label>
+                                <input type="time" id="hora_inicio" name="start_time" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="hora_fin">Hora de Fin:</label>
+                                <input type="time" id="hora_fin" name="end_time" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="privacidad">Privacidad:</label>
+                                <select id="privacidad" name="privacy" class="form-control">
+                                    <option value="Pública">Pública</option>
+                                    <option value="Privada">Privada</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label para="relacion_acta">Relacionar con Acta:</label>
+                                <select id="relacion_acta" name="relationship_record" class="form-control">
+                                    <option value="">Ninguna</option>
+                                    <?php
+                                    $actas = $recordController->getAllRecords();
+                                    foreach ($actas as $acta) {
+                                        echo "<option value='" . $acta["id_record"] . "'>" . $acta["affair"] . " - Responsable: " . $acta["responsible"] . "</option>";
+                                    }
+                                    ?>
+                                </select>
+
+                            </div>
+                            <button type="submit" class="btn btn-primary">Agregar Acta</button>
+                        </form>
+                    </div>
                 </div>
 
                 <div class="main-cards">
-
-                <div class="card">
-                    <div class="card-inner">
-                        <h3>ACTAS</h3>
-                        <span class="material-icons-outlined">fact_check</span>
-                    </div>
                     <?php
-                    $actas = $recordController->getAllRecords(); 
-                    $tamano = count($actas);
-                    echo "<h1>".$tamano."</h1>";
+                    $actas = $recordController->getAllRecords();
+                    $responsable = $userController->getUserByID($id['id_user']);
+                    foreach ($actas as $acta) {
+                        echo '<div class="card" data-meeting-id="' . $acta['id_record'] . '">
+                            <div class="card-inner">
+                                <div><h4>Nombre:</h4><h3>' . $acta['affair'] . '</h3></div>
+                                <div><h4>Fecha:</h4><h4>' . $acta['date_record'] . '</h4></div>
+                                <div><h4>Hora de inicio:</h4><h4>' . $acta['start_time'] . ' - ' . $acta['end_time'] . '</h4></div>
+                                <h4>Responsable: ' . $responsable['name'] . '</h4>
+                            </div>
+                        </div>';
+                    }
+
+
                     ?>
-                </div>
-
-                <div class="card">
-                    <div class="card-inner">
-                        <h3>REUNIONES</h3>
-                        <span class="material-icons-outlined">camera</span>
-                    </div>
-                    <?php
-                    $actas = $recordController->getAllRecords(); 
-                    $tamano = count($actas);
-                    echo "<h1>".$tamano."</h1>";
-                    ?>
-                </div>
-
-                <div class="card">
-                    <div class="card-inner">
-                        <h3>USUARIOS</h3>
-                        <span class="material-icons-outlined">groups</span>
-                    </div>
-                    <?php
-                    $actas = $recordController->getAllRecords(); 
-                    $tamano = count($actas);
-                    echo "<h1>".$tamano."</h1>";
-                    ?>
-                </div>
-
-                <div class="card">
-                    <div class="card-inner">
-                        <h3>ALERTAS</h3>
-                        <span class="material-icons-outlined">notifications</span>
-                    </div>
-                    <?php
-                    $actas = $recordController->getAllRecords(); 
-                    $tamano = count($actas);
-                    echo "<h1>".$tamano."</h1>";
-                    ?>
-                </div>
-
-                </div>
-
-                <div class="charts">
-
-                    <div class="charts-card">
-                        <h2 class="charts-title">Las 5 mejores actas</h2>
-                        <div id="bar-chart"></div>
-                    </div>
-
-                    <div class="charts-card">
-                        <h2 class="charts-title">Participantes por reunion</h2>
-                        <div id="area-chart"></div>
-                    </div>
 
                 </div>
             </main>
-            </div>
-            <!-- <h1>Gestión de Actas</h1>
-
-             Formulario para agregar una nueva acta
-            <h2>Agregar Acta</h2>
-            <form id="agregar-acta-form">
-                <div class="form-group">
-                    <label for="asunto">Asunto:</label>
-                    <input type="text" id="asunto" name="asunto" class="form-control" required>
-                </div>
-                <div class="form-group">
-                    <label for="responsable">Responsable (ID):</label>
-                    <input type="number" id="responsable" name="responsable" class="form-control" required>
-                </div>
-                <div class="form-group">
-                    <label for="fecha">Fecha:</label>
-                    <input type="date" id="fecha" name="fecha" class="form-control" required>
-                </div>
-                <div class="form-group">
-                    <label for="hora_inicio">Hora de Inicio:</label>
-                    <input type="time" id="hora_inicio" name="hora_inicio" class="form-control" required>
-                </div>
-                <div class="form-group">
-                    <label for="hora_fin">Hora de Fin:</label>
-                    <input type="time" id="hora_fin" name="hora_fin" class="form-control" required>
-                </div>
-                <div class="form-group">
-                    <label for="privacidad">Privacidad:</label>
-                    <select id="privacidad" name="privacidad" class="form-control">
-                        <option value="Pública">Pública</option>
-                        <option value="Privada">Privada</option>
-                    </select>
-                </div>
-                 Campo para establecer la relación entre actas 
-                <div class="form-group">
-                    <label para="relacion_acta">Relacionar con Acta:</label>
-                    <select id="relacion_acta" name="relacion_acta" class="form-control">
-                        <option value="">Ninguna</option>
-                        <?php
-                        $actas = $recordController->getAllRecords();
-                        // Agregar opciones de actas existentes
-                        foreach ($actas as $acta) {
-                            echo "<option value='" . $acta["id_record"] . "'>" . $acta["affair"] . " - Responsable: " . $acta["responsible"] . "</option>";
-                        }
-                        ?>
-                    </select>
-                </div>
-                <button type="submit" class="btn btn-primary">Agregar Acta</button>
-            </form>
-
-             Área para mostrar las actas existentes
-            <div class="container mt-5">
-                <h1>Lista de Actas</h1>
-
-                <?php if (empty($actas)) { ?>
-                    <p>No se encontraron actas.</p>
-                <?php } else { ?>
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Fecha</th>
-                                <th>Hora de Inicio</th>
-                                <th>Hora de Fin</th>
-                                <th>Asunto</th>
-                                <th>Responsable</th>
-                                <th>Privacidad</th>
-                                <th>Relación</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($actas as $acta) { ?>
-                                <tr>
-                                    <td><?php echo $acta['id_record']; ?></td>
-                                    <td><?php echo $acta['date_record']; ?></td>
-                                    <td><?php echo $acta['start_time']; ?></td>
-                                    <td><?php echo $acta['end_time']; ?></td>
-                                    <td><?php echo $acta['affair']; ?></td>
-                                    <td><?php echo $acta['responsible']; ?></td>
-                                    <td><?php echo $acta['privacy']; ?></td>
-                                    <td><?php echo isset($acta['relationship_record']) ? $acta['relationship_record'] : 'Ninguna'; ?></td>
-                                </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
-                <?php } ?>
-            </div> -->
-
-            <!-- JavaScript para manejar formularios y solicitudes AJAX -->
-            <script>
-                // Manejo de formulario para agregar una nueva acta
-                $('#agregar-acta-form').submit(function (event) {
-                    event.preventDefault(); // Evitar la recarga de la página
-
-                    const datos = {
-                        asunto: $('#asunto').val(),
-                        responsable: $('#responsable').val(),
-                        fecha: $('#fecha').val(),
-                        hora_inicio: $('#hora_inicio').val(),
-                        hora_fin: $('#hora_fin').val(),
-                        privacidad: $('#privacidad').val(),
-                        relacion_acta: $('#relacion_acta').val(),
-                    };
-
-                    // Enviar datos a través de AJAX al archivo PHP
-                    $.post('index.php', datos, function (response) {
-                        alert(response); // Mostrar el resultado
-                    });
-                });
-            </script>
         </div>
 
         <!-- Scripts -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/3.35.5/apexcharts.min.js"></script>
         <script src="scripts.js"></script>
+        <script>
+            window.onclick = function (event) {
+                var modal = document.getElementById('modalForm');
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+                var userSearchModal = document.getElementById('userSearchModal');
+                if (event.target == userSearchModal) {
+                    userSearchModal.style.display = "none";
+                }
+            }
+
+            document.addEventListener('DOMContentLoaded', (event) => {
+                // Asegúrate de que todos los elementos DOM estén cargados antes de agregar los event listeners
+                document.querySelectorAll('.card').forEach(card => {
+                    card.addEventListener('click', function () {
+                        const meetingId = this.getAttribute('data-meeting-id');
+                        openUserSearchModal(meetingId);
+                    });
+                });
+            });
+
+            function openUserSearchModal(meetingId) {
+                document.getElementById('userSearchModal').style.display = 'block';
+                // Almacenar el ID de la reunión para agregar participantes
+                document.getElementById('userSearchModal').setAttribute('data-meeting-id', meetingId);
+            }
+
+            function searchUsers() {
+                var input = document.getElementById('userSearch').value;
+                var userList = document.getElementById('userList');
+                userList.innerHTML = '';
+
+                if (input.length > 2) {
+                    fetch('index.php?route=participants&query=' + input)
+                        .then(response => response.json())
+                        .then(data => {
+                            data.forEach(user => {
+                                var li = document.createElement('li');
+                                li.textContent = user.name + ' (' + user.email + ')';
+                                li.setAttribute('data-user-id', user.id);
+                                li.onclick = function () {
+                                    selectUser(user.id, user.name, user.email);
+                                };
+                                userList.appendChild(li);
+                            });
+                        });
+                }
+            }
+
+            function selectUser(userId, userName, userEmail) {
+                var selectedUsers = document.getElementById('selectedUsers');
+                var li = document.createElement('li');
+                li.textContent = userName + ' (' + userEmail + ')';
+                li.setAttribute('data-user-id', userId);
+                selectedUsers.appendChild(li);
+            }
+
+            function addParticipant() {
+                var selectedUsers = document.getElementById('selectedUsers').children;
+                var meetingId = document.getElementById('userSearchModal').getAttribute('data-meeting-id');
+
+                var userIds = [];
+                for (var i = 0; i < selectedUsers.length; i++) {
+                    userIds.push(selectedUsers[i].getAttribute('data-user-id'));
+                }
+
+                fetch('index.php?route=participants', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        meeting_id: meetingId,
+                        user_ids: userIds
+                    })
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.success) {
+                            alert('Participantes agregados exitosamente.');
+                            document.getElementById('userSearchModal').style.display = 'none';
+                            location.reload();
+                        } else {
+                            alert('Hubo un error al agregar los participantes.');
+                        }
+                    });
+            }
+
+        </script>
     </body>
 
     </html>
-
     <?php
 } else {
     header("Location: home.html");
